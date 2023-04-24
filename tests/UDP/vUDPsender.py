155541aad0b583599e344dc235681eb1aeab5de0
thomasmatre <thomas.matre@hotmail.com>
2023-04-24 12:01:15 +0200

    updated tests

--- tests/UDP/vUDPsender.py
+++ tests/UDP/vUDPsender.py
@@ -1,16 +1,16 @@
 import cv2, socket, pickle, os
 import numpy as np
+BUFF_SIZE = 65536
 s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET,socket.SO_SNDBUF,1000000)
-server_ip = "10.0.0.12"
-server_port = 6666
+s.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+server_ip = "0.0.0.0"
+server_port = 5000
 
 cap = cv2.VideoCapture(2)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1024)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 while True:
 	ret,photo = cap.read()
-	#cv2.imshow('streaming',photo)
 	ret,buffer = cv2.imencode(".jpg",photo,[int(cv2.IMWRITE_JPEG_QUALITY),30])
 	x_as_bytes = pickle.dumps(buffer)
 	s.sendto((x_as_bytes),(server_ip,server_port))
